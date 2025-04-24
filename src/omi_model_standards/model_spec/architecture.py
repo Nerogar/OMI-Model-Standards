530a1a22362febcc2c001e52ae685907d05e163d
Justin Tappeser <nerogar@arcor.de>
2025-04-24 21:59:26 +0200

    initial version of lora conversion functions
    Signed-off-by: Justin Tappeser <nerogar@arcor.de>

---
+++ src/omi_model_standards/model_spec/architecture.py
@@ -0,0 +1,25 @@
+stable_diffusion_1_lora = 'stable-diffusion-v1/lora'
+stable_diffusion_1_inpainting_lora = 'stable-diffusion-v1-inpainting/lora'
+stable_diffusion_2_lora = 'stable-diffusion-v2/lora'
+stable_diffusion_2_inpainting_lora = 'stable-diffusion-v2-inpainting/lora'
+
+stable_diffusion_3_lora = 'stable-diffusion-v3/lora'
+stable_diffusion_35_lora = 'stable-diffusion-v3.5/lora'
+
+stable_diffusion_xl_1_lora = 'stable-diffusion-xl-v1-base/lora'
+stable_diffusion_xl_1_inpainting_lora = 'stable-diffusion-xl-v1-base-inpainting/lora'
+
+wuerstchen_2_lora = 'wuerstchen-v2-prior/lora'
+stable_cascade_1_lora = 'stable-cascade-v1-prior/lora'
+
+pixart_alpha_lora = 'pixart-alpha/lora'
+pixart_sigma_lora = 'pixart-sigma/lora'
+
+flux_dev_1_lora = 'Flux.1-dev/lora'
+flux_fill_dev_1_lora = 'Flux.1-fill-dev/lora'
+
+sana_lora = 'sana/embedding'
+
+hunyuan_video_lora = 'hunyuan-video/lora'
+
+hi_dream_i1_lora = 'hi-dream-i1/lora'
