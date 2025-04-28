7cb6d66440979eb9cae34ca409ac1f0f8dfa23aa
Justin Tappeser <nerogar@arcor.de>
2025-04-28 22:53:01 +0200

    add hidream lora key conversion
    Signed-off-by: Justin Tappeser <nerogar@arcor.de>

---
+++ src/omi_model_standards/convert/lora/convert_hidream_lora.py
@@ -0,0 +1,14 @@
+from omi_model_standards.convert.lora.convert_lora_util import LoraConversionKeySet, map_prefix_range
+
+
+def convert_hidream_lora_key_sets() -> list[LoraConversionKeySet]:
+    keys = []
+
+    keys += [LoraConversionKeySet("bundle_emb", "bundle_emb")]
+    keys += [LoraConversionKeySet("transformer", "lora_transformer")]
+    keys += [LoraConversionKeySet("clip_l", "lora_te1")]
+    keys += [LoraConversionKeySet("clip_g", "lora_te2")]
+    keys += [LoraConversionKeySet("t5", "lora_te3")]
+    keys += [LoraConversionKeySet("llama", "lora_te4")]
+
+    return keys
